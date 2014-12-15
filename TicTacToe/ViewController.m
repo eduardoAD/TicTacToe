407ad9a5d2f642dbdc599d84d7826a22e49d8b7c
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-12-15 12:56:25 -0600

    task #4 complete
    As a user, I want to determine if user has won the game

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -19,9 +19,13 @@
 @property (strong, nonatomic) IBOutlet UILabel *labelEight;
 @property (strong, nonatomic) IBOutlet UILabel *labelNine;
 @property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (strong, nonatomic) IBOutlet UILabel *winnerText;
 
 @property NSArray *labelsArray;
+@property NSArray *combinationsToWin;
 @property (nonatomic, assign, getter=isTurnX) BOOL turnX;
+@property (nonatomic, assign, getter=isGameOver) BOOL gameOver;
+@property int turn;
 
 @end
 
@@ -29,9 +33,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.turn = 0;
     self.turnX = YES;
     [self setTurnText:self.whichPlayerLabel];
+    self.winnerText.text = @"";
     self.labelsArray = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
+    self.combinationsToWin = [[NSArray alloc] initWithObjects:
+                              @[self.labelOne, self.labelTwo, self.labelThree],
+                              @[self.labelFour, self.labelFive, self.labelSix],
+                              @[self.labelSeven, self.labelEight, self.labelNine],
+                              @[self.labelOne, self.labelFour, self.labelSeven],
+                              @[self.labelTwo, self.labelFive, self.labelEight],
+                              @[self.labelThree, self.labelSix, self.labelNine],
+                              @[self.labelOne, self.labelFive, self.labelNine],
+                              @[self.labelThree, self.labelFive, self.labelSeven], nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -51,6 +66,7 @@
 
 -(void)changeTurn{
     self.turnX = !self.turnX;
+    self.turn++;
     [self setTurnText:self.whichPlayerLabel];
 }
 
@@ -72,7 +88,34 @@
     UILabel *label = [self findLabelUsingPoint:tapPoint];
     if (label != nil) {
         [self setTurnText:label];
-        [self changeTurn];
+        [self checkWhoWon];
+        if (![self isGameOver]) {
+            [self changeTurn];
+        }
+    }
+}
+
+-(NSString *)whoWon{
+    for (NSArray *combo in self.combinationsToWin) {
+        int count = 0;
+        for (UILabel *label in combo) {
+            if ([label.text isEqualToString:self.whichPlayerLabel.text]) {
+                count ++;
+                if (count == 3) {
+                    return [NSString stringWithFormat:@"%@ has won :)",self.whichPlayerLabel.text];
+                }
+            }
+        }
+    }
+    return @"";
+}
+
+-(void)checkWhoWon{
+    self.winnerText.text = [self whoWon];
+    if ([self.winnerText.text isEqualToString:@""]) {
+        self.gameOver= NO;
+    }else{
+        self.gameOver = YES;
     }
 }
 
