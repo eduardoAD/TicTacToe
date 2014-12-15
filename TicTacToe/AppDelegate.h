a95a7362f2b6d5b973eff965e458fa8447f089d5
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-12-15 11:02:27 -0600

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Eduardo Alvarado Díaz on 12/15/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Eduardo Alvarado Díaz on 12/15/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
