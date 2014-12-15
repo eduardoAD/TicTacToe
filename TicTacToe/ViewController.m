4285b8bc24962c2eab17682d002f90f9a208a8bf
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-12-15 11:10:41 -0600

    Task #1 complete
    As a user, I want to view playing board

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,6 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *labelOne;
+@property (strong, nonatomic) IBOutlet UILabel *labelTwo;
+@property (strong, nonatomic) IBOutlet UILabel *labelThree;
+@property (strong, nonatomic) IBOutlet UILabel *labelFour;
+@property (strong, nonatomic) IBOutlet UILabel *labelFive;
+@property (strong, nonatomic) IBOutlet UILabel *labelSix;
+@property (strong, nonatomic) IBOutlet UILabel *labelSeven;
+@property (strong, nonatomic) IBOutlet UILabel *labelEight;
+@property (strong, nonatomic) IBOutlet UILabel *labelNine;
 
 @end
 
